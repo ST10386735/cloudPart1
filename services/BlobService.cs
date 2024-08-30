0178e548a7b60048df70cc021da6afb6a8803b18
ST10386735 <131518002+ST10386735@users.noreply.github.com>
2024-08-30 11:46:59 +0200

    Add project files.

---
+++ services/BlobService.cs
@@ -0,0 +1,22 @@
+﻿/*namespace WebApplication3.services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            _blobServiceClient = new BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream content)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(content, true);
+        }
+    }
+
+}
+*/
